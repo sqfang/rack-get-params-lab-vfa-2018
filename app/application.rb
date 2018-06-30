db798effb0fc8779a907aaa3ea7253629a49365c
Sophia Fang <sophiaqfang@gmail.com>
2018-06-30 09:14:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,8 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
+      
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
