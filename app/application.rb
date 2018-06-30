8ab94c9efa26f114f98388e618969499f091a71d
Sophia Fang <sophiaqfang@gmail.com>
2018-06-30 09:27:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,6 @@ class Application
       else
         resp.write "We don't have that item"
       end
-    end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
