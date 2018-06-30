337a2d2fa4646bcc8c4cd222495eb3e793c215ef
Sophia Fang <sophiaqfang@gmail.com>
2018-06-30 09:23:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,7 @@ class Application
       if @@items.include?(item)
         resp.write 
       else
-        
+        resp.write "We don't have that item"
       end 
     elsif req.path.match(/search/)
       search_term = req.params["q"]
